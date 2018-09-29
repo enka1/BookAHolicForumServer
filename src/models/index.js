f9651d763bc34c3552a9966f26e86b3dee6122f6
unknown <nhithanh123vnn@gmail.com>
2018-09-29 16:07:42 +0700

    setup

--- src/models/index.js
+++ src/models/index.js
@@ -5,6 +5,6 @@ import PostModel from './Post.model'
 import CommentModel from './Comment.model'
 
 export const connectMongoDB = () => {
-  mongoose.connect('mongodb://localhost:27017/Forum', {useNewUrlParser: true})
+  mongoose.connect(process.env.MLAB_URL ? process.env.MLAB_URL : 'mongodb://localhost:27017/Forum', {useNewUrlParser: true})
 }
 export {UserModel, PostModel, CommentModel}
