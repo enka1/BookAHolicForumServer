4316362d66e2ba86fd69656051871eccf3c25ad3
unknown <nhithanh123vnn@gmail.com>
2018-09-29 17:06:03 +0700

    deploy

--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,5 @@ export const jwtSecret = 'test123'
 export const googleOauthConfig = {
     clientID: '789642630681-4dclhqlfli0jfjqbu0atg07oi517g4dq.apps.googleusercontent.com',
     clientSecret: '4qlHj0zm5DdM_JKaVZf_wAC0',
-    callbackURL: 'http://localhost:3001/auth/google/callback'
+    callbackURL: process.env.GOOGLE_CALLBACK ? process.env.GOOGLE_CALLBACK : 'http://localhost:3001/auth/google/callback'
 }
